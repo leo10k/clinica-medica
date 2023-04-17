3bed4b8f024bcbbae104bee0c380c605a82c537f
leo10k <leozinhomacedo41@gmail.com>
2023-04-17 20:52:41 -0300

    adding the method put at the PacienteController, and changing little things

--- src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -2,10 +2,10 @@ package med.voll.api.paciente;
 
 import org.springframework.data.web.PageableDefault;
 
-public record DadosListagemPaciente(String nome, String email, String cpf, String telefone) {
+public record DadosListagemPaciente(Long id, String nome, String email, String cpf, String telefone) {
 
     public DadosListagemPaciente(Paciente paciente) {
-        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone());
+        this(paciente.getId(),paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone());
     }
 
 }
