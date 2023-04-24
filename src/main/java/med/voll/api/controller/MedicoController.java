60e117d5ee153c07ac7073fbe42de77042618e0b
leo10k <leozinhomacedo41@gmail.com>
2023-04-24 05:18:22 -0300

    colocando metodo get by id no MedicoController

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -57,4 +57,10 @@ public class MedicoController {
         return ResponseEntity.noContent().build();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedio(medico));
+    }
+
 }
