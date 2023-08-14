ff12395a821523c2911117f810f2738d6f7f350a
leo10k <leozinhomacedo41@gmail.com>
2023-08-14 17:55:01 -0300

    creating reason for schedule cancellation classes

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,20 +1,19 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.domain.consulta.AgendaDeConsultas;
-import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
-import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("consultas")
 public class ConsultaController {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
 
     @Autowired
     private AgendaDeConsultas agenda;
@@ -24,6 +23,12 @@ public class ConsultaController {
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
         agenda.agendar(dados);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados) {
+        agenda.cancelar(dados);
+        return ResponseEntity.noContent().build();
     }
 
 }
